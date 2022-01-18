1c9a4976facffe46ce412e07afbcee869eb494e1
BluBack <kangrokristo1@gmail.com>
2022-01-18 08:46:21 +0200

    tööd

---
+++ praks3/kasutaja.py
@@ -0,0 +1,16 @@
+class Kasutaja():
+ def __init__(self, first_name, surename, GPU):
+        self.first_name = first_name
+        self.surename = surename
+        self.GPU = GPU
+
+   def tervita_kasutaja(self):
+        print("Tere, " + str(self.first_name) + " " + str(self.surename))
+
+    def kirjalda_Kasutaja(self):
+        print("Kasutaja andmed: ")
+        print("eesnimi: " str(self.first_name))
+        print("eesnimi: " str(self.surename))
+
+def kirjalda_Kasutaja_GPUd(self):
+    print("kasutaja GPU on: "str(self.GPU))
---
+++ praks3/kasutajad.py
@@ -0,0 +1,25 @@
+from praks3.kasutaja import Kasutaja
+
+kasutaja1 = Kasutaja()
+Kasutaja1.first_name = "Kristo"
+Kasutaja1.surename = "Kangro"
+Kasutaja3.GPU = "RTX 2080 s"
+Kasutaja1.kirjalda_Kasutaja_GPUd()
+Kasutaja1.kirjelda_kasutaja()
+Kasutaja1.tervita_kasutajat()
+
+kasutaja2 = Kasutaja()
+Kasutaja2.first_name = "Tanel"
+Kasutaja2.surename = "Tamm"
+Kasutaja3.GPU = "   GTX 1080 ti"
+Kasutaja2.kirjalda_Kasutaja_GPUd()
+Kasutaja2.kirjelda_kasutaja()
+Kasutaja2.tervita_kasutajat()
+
+kasutaja3 = Kasutaja()
+Kasutaja3.first_name = "Kevin"
+Kasutaja3.surename = "Mägi"
+Kasutaja3.GPU = "RTX 2060"
+Kasutaja3.kirjalda_Kasutaja_GPUd()
+Kasutaja3.kirjelda_kasutaja()
+Kasutaja3.tervita_kasutajat()
