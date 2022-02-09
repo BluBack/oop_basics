723844ed128fa51c8dd84c96d1d58bd52dc7716e
BluBack <kangrokristo1@gmail.com>
2022-02-09 14:24:38 +0200

    tööd

--- praks5/kontod.py
+++ praks5/kontod.py
@@ -4,14 +4,14 @@ paul_konto = Konto("Paul", 100.00)
 jakob_konto = Konto("Jakob", 500.00)
 
 print("Algne saldo")
-print(paul_konto.naita_saldo())  # 100.0
-print(jakob_konto.naita_saldo())  # 500.0
+print(paul_konto.naita_saldo())
+print(jakob_konto.naita_saldo())
 
 jakob_konto.ylekanne(250.00)
-print("Jakobi saldo nüüd on {0}".format(jakob_konto.naita_saldo()))  # Jakobi saldo nüüd on 250.0
+print("Jakobi saldo nüüd on {0}".format(jakob_konto.naita_saldo()))
 paul_konto.deposiit(250.00)
-print("Pauli saldo nüüd on {0}".format(paul_konto.naita_saldo()))  # Pauli saldo nüüd on 350.0
+print("Pauli saldo nüüd on {0}".format(paul_konto.naita_saldo()))
 
 print("Lõplik seisund")
-print(paul_konto.naita_saldo())  # 350.0
-print(jakob_konto.naita_saldo())  # 250.0
+print(paul_konto.naita_saldo())
+print(jakob_konto.naita_saldo())
