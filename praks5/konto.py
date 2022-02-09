23ff7266db08440715f6adb57c7b70567d2afcd2
BluBack <kangrokristo1@gmail.com>
2022-02-09 12:33:37 +0200

    tööd

---
+++ praks5/konto.py
@@ -0,0 +1,18 @@
+class Konto:
+    def init(self, nimi, saldo):
+        self.nimi = nimi
+        self.raha = saldo
+
+    def ylekanne(self, kogus):
+        summa = self.raha - kogus
+        print("Te tegite ülekande suurusega " + str(summa))
+
+    def deposiit(self, kogus):
+       summa = self.raha + kogus
+       print("Te depositisite " + str(summa))
+
+    def naita_saldo(self):
+        return str(self.raha) + " eurot"
+
+    def naita_nimi(self):
+        print("Teie nimi on: " + str(self.nimi))
